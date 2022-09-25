54174ecf694c9a57cd903e9d34fa5f67694bb51e
otcova <otgercomas@gmail.com>
2022-09-25 12:09:57 +0200

    js report error

--- rust-wasm-webgl/src/error.rs
+++ rust-wasm-webgl/src/error.rs
@@ -1,3 +1,11 @@
+use crate::console;
+
 pub fn fmt<E: std::fmt::Debug>(error: E, msg: &str) -> String {
     format!("{}. Details: {:?}", msg, error)
+}
+
+pub fn report<T>(result: Result<T, String>) {
+    if let Err(error) = result {
+        console::log!(" ----- [ERROR] -----\n{}", error);
+    }
 }
